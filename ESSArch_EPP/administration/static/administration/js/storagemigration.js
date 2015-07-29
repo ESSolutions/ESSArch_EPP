6f3475ac63d64cec79aac15177818aa944b33798
Soren Norrby <info@sorennorrby.com>
2015-07-29 15:26:37 +0200

    correction of copypath variable

--- ESSArch_EPP/administration/static/administration/js/storagemigration.js
+++ ESSArch_EPP/administration/static/administration/js/storagemigration.js
@@ -245,7 +245,7 @@
 							if (document.getElementById("copyonlyflag").checked == true){
 								data = "Copy  Only"
 								if ($('#copypath').val() == ""){
-									$('#copypath').val() = prompt(" You must tell us the Copy Path")
+									var answer = prompt(" You must tell us the Copy Path")
 								}
 								
 							}
