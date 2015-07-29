52ac70d5b9adb17cc1896b32c9c2a874b710e86a
Soren Norrby <info@sorennorrby.com>
2015-07-29 13:09:49 +0200

    eventlist with eventDescription

--- ESSArch_EPP/reports/views.py
+++ ESSArch_EPP/reports/views.py
@@ -106,7 +106,7 @@ class eventsReport(ListView):
         event_list = []
         for i in eventobject_list:
             b = ""
-            b = str(i.eventType) + " " + i.eventDetail
+            b = str(i.eventType) + " " + eventCodesDict[i.eventType]
             event_list.append(b)
         
         #print '#####################################: %s' % str(event_list)
