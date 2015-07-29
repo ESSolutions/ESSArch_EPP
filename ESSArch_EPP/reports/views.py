93a5b6467488dc35a1d048f9063fef8a4e4de771
Soren Norrby <info@sorennorrby.com>
2015-07-29 14:04:40 +0200

    corrected that i was turned into eventType twise

--- ESSArch_EPP/reports/views.py
+++ ESSArch_EPP/reports/views.py
@@ -110,7 +110,7 @@ class eventsReport(ListView):
         #print '#####################################: %s' % str(event_list)
         event_list2 = []
         for i in list(set(event_list)):
-            event_list2.append([i,eventCodesDict[int(i.eventType)], event_list.count(i)])
+            event_list2.append([i,eventCodesDict[int(i)], event_list.count(i)])
             
             #event_list2.append([i,event_list[i].eventDetail])
         #print '#####################################: %s' % str(event_list2)
