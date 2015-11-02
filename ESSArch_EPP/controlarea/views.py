bb132adc48d80bd912cc120031ddd79af4799db7
Soren Norrby <info@sorennorrby.com>
2015-11-02 15:15:13 +0100

    Altered Test Task to use TestString

--- ESSArch_EPP/controlarea/views.py
+++ ESSArch_EPP/controlarea/views.py
@@ -1753,8 +1753,8 @@ class TestTaskView(TemplateView):
     def get_context_data(self, **kwargs):
 
         context = super(TestTaskView, self).get_context_data(**kwargs)
-        TestString= 'Teststring'
-        TestToSeeTasks = TestTask.delay_or_fail(TestString=TestString)
+        TestString = 'Teststring'
+        TestToSeeTasks = TestTask.delay_or_fail(TestString)
         context['task_id'] = TestToSeeTasks.task_id
         context['result'] = TestToSeeTasks.result
         return context
