3222863274b09069be49d52666fa2bcfaab5089e
Soren Norrby <info@sorennorrby.com>
2015-11-17 11:09:55 +0100

    Created Javascript library to handle tasks in progress.

--- ESSArch_EPP/controlarea/static/controlarea/js/progress.js
+++ ESSArch_EPP/controlarea/static/controlarea/js/progress.js
@@ -1,37 +1,7 @@
-{% extends "admin/base_site.html" %}
-{% load js staticfiles%}
-
-{% block content %}
-Request to Diffcheck IP
-<br>
-<br>
-<div id="taskinprogress">
-</div>
-<br>
-  
-{% endblock %}
-{% block app-extra-script %}
-     {% js "controlarea/js/progress.js" %}
-<script>
-//Reload script
-window.onload = setupRefresh;
-
-function setupRefresh() {
-  setTimeout("refreshPage();", 24000);
-}
-function refreshPage() {
-   window.location = location.href;
-}
-
-
-var taskid = "{{ taskid }}";
-console.log(taskid);
-
-getTaskInfo(taskid);
-
-/*
 function getTaskInfo(taskid){
 
+	console.log('Get task info called');
+	
 var taskinprogressinfo = {};
 $.getJSON( '/task/' + taskid + '/status/', function(taskinprogressinfo){
 
@@ -55,6 +25,9 @@ function determineStatus(thetask){
             case 'FAILURE':
                 failedtask(taskid);
                 break;
+            case 'notaskfound':
+                return infohtml;
+                break;            
             case 'PENDING':
                 pendingtask(taskresult);
                 break;
@@ -76,25 +49,33 @@ function failedtask(taskid){
             
     $.getJSON( '/controlarea/taskresult/' + taskid + '/', function(failedtaskinfo1){
         	        
-        	var readable = getfailedtaskhtml(failedtaskinfo1);
+        	
+            console.log('Failed task info');
+            console.log(failedtaskinfo1);
+            var readable = getfailedtaskhtml(failedtaskinfo1);
         	infohtml = readable;;
         	 document.getElementById("taskinprogress").innerHTML = infohtml;
         	 });
-        	    
-   };
+        	//return infohtml;    
+   }
 
 function pendingtask(result){
 
-    infohtml = '<b>The request is pending<b><br>' + result;
+    infohtml = '<b>The request is pending<b><br>';
     return infohtml;
 }
 
 function progresstask(result){
     
+	 infohtml = 'Request is in progress<br><br>';
     var progressresult = result['progress_percent'];
-    infohtml = 'Request is in progress<br><br><progress value=' + progressresult + ' max="100"></progress>';
+	 if(progressresult != undefined){
     console.log('progress percent');
     console.log(progressresult);
+    infohtml = infohtml + '<progress value=' + progressresult + ' max="100"></progress>';
+    console.log('progresshtml');
+    console.log(infohtml);
+	 }
     return infohtml;
 
 }
@@ -125,7 +106,7 @@ function successtask(result){
     
     var statusdetail = result['statusdetail'];
     console.log(statusdetail);
-    if (statusdetail.length > 0){
+    if (statusdetail != undefined){
     var statusdetailhtml = '<br>Status info<br><table>';
     for (i = 0; i < statusdetail.length; i++){
     statusdetailhtml = statusdetailhtml + '<tr><td>' + statusdetail[i] + '<td></tr>';
@@ -156,9 +137,76 @@ document.getElementById("taskinprogress").innerHTML = taskhtml;
 
 };
 
+
 };
-*/
 
-</script>
+function getfailedtaskhtml(failinfo){
 
-{% endblock %}
+var failedtask = failinfo['task'];
+
+var failresult = failedtask['result'];
+
+var  failedTaskshtml = '<b>The request failed</b><br>';
+var typeoferror = failresult['py/object'];
+
+var typeoferror = failresult['py/object'];
+var errortype = typeoferror;
+var reduce = failresult['py/reduce'];
+if(typeoferror =='controlarea.tasks.ControlareaException'){
+	
+	console.log('This is a known error');
+	errortype = 'Controlarea'
+
+var getmore = reduce[1];
+	console.log('getmore');
+	console.log(getmore);
+var ourtuple = getmore['py/tuple'];
+	console.log('our tuple');
+	console.log(ourtuple)
+var moreinfo = ourtuple[0];
+	console.log('moreinfo');
+	console.log(moreinfo);
+    var taskcategory = moreinfo['category'];
+    console.log('taskcategory');
+    console.log(taskcategory);
+    var tasklabel = moreinfo['label'];
+    console.log('tasklabel');
+    console.log(tasklabel);
+    if(tasklabel == 'Test task'){
+    	taskstatuslist = ['Test1','Test2','Test3'];
+    }
+    
+    else{
+    taskstatuslist = moreinfo['statuslist'];
+    console.log('taskstatuslist');
+    console.log(taskstatuslist);
+    }
+	var taskrequestpurpose = moreinfo['reqpurpose'];
+	console.log('taskrequestpurpose');
+	console.log(taskrequestpurpose);
+	var taskuser = moreinfo['user'];
+	console.log('taskuser');
+   console.log(taskuser);
+    var ipuuid = moreinfo['ipuuid'];
+	var taskerrorlist = [];
+	if(tasklabel == 'Test task'){
+		taskerrorlist = 'Some random error';
+	}
+	else{
+		taskerrorlist = moreinfo['errorlist'][0];
+	}
+		
+	//failedTaskshtml =  failedTaskshtml +  'Controlareaexception';
+	failedTaskshtml = failedTaskshtml + '<br>'+ 'FAILURE' + '<br>' + 'Controlarea' +  '<br><br>' + '<b>IP UUID: </b>' + ipuuid + '<br>'+ '<b> Request purpose: </b>'+ taskrequestpurpose + '<br><b> Action:</b> ' + tasklabel + '<br><b> Errorlist: </b> ' + taskerrorlist;
+
+}
+
+else{
+//failedTaskshtml =  failedTaskshtml + '<tr><td>' + failedtasks[i].status + '</td><td>' + errortype + '</td><td>' + failedtasks[i].result + '</td></tr>';
+failedTaskshtml = failedTaskshtml + '<br>' + 'FAILURE' + '<br><br>' + errortype + '<br><br>' + reduce[1]['py/tuple'];
+
+}
+console.log(failedTaskshtml);
+return failedTaskshtml;
+	
+};
