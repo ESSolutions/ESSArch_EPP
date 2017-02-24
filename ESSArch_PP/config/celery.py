cb5191606f4fdb721f0d76fe8336731648aa163c
Oskar Persson <oskar.persson@polken.se>
2017-02-24 13:41:28 +0100

    Remove debug task

--- ESSArch_PP/config/celery.py
+++ ESSArch_PP/config/celery.py
@@ -10,7 +10,3 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 app = Celery('config')
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
-
-@app.task(bind=True)
-def debug_task(self):
-    print('Request: {0!r}'.format(self.request))
