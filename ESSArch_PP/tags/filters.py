49a32b8cf9c686d2da5eec6c501c49c2ec997659
Oskar Persson <oskar.persson@polken.se>
2017-02-27 15:13:45 +0100

    Add tag filter for tree roots

--- ESSArch_PP/tags/filters.py
+++ ESSArch_PP/tags/filters.py
@@ -8,10 +8,17 @@ from ESSArch_Core.tags.models import Tag
 
 class TagFilter(django_filters.FilterSet):
     include_leaves = django_filters.BooleanFilter(method='filter_leaves', widget=BooleanWidget())
+    only_roots = django_filters.BooleanFilter(method='filter_roots', widget=BooleanWidget())
 
     def filter_leaves(self, queryset, name, value):
         if not value:
             return queryset.exclude(lft=F('rght')-1)
+
+        return queryset
+
+    def filter_roots(self, queryset, name, value):
+        if value:
+            return queryset.filter(parent__isnull=True)
 
         return queryset
 
