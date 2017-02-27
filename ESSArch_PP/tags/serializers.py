6dcc798d6a34c7ee9c3f1e8f4c07725be3f14dd6
Oskar Persson <oskar.persson@polken.se>
2017-02-27 14:44:23 +0100

    Add serializer for nested tags

--- ESSArch_PP/tags/serializers.py
+++ ESSArch_PP/tags/serializers.py
@@ -4,7 +4,14 @@ from ESSArch_Core.ip.models import InformationPackage
 from ESSArch_Core.tags.models import Tag
 
 
+class NestedTagSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ('url', 'id', 'name', 'desc',)
+
+
 class TagSerializer(serializers.HyperlinkedModelSerializer):
+    children = NestedTagSerializer(many=True, read_only=True)
     information_packages = serializers.HyperlinkedRelatedField(
         many=True,
         queryset=InformationPackage.objects.all(),
