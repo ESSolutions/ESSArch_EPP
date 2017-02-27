baf81731307e8a4a9f14a60da0a7966f8fa18a37
Oskar Persson <oskar.persson@polken.se>
2017-02-27 14:05:26 +0100

    Add route for information-packages to tag view

--- ESSArch_PP/tags/views.py
+++ ESSArch_PP/tags/views.py
@@ -1,7 +1,11 @@
 from rest_framework import viewsets
+from rest_framework.decorators import detail_route
+from rest_framework.response import Response
 
+from ESSArch_Core.ip.models import InformationPackage
 from ESSArch_Core.tags.models import Tag
 
+from ip.serializers import InformationPackageSerializer
 from tags.serializers import TagSerializer
 
 
@@ -11,3 +15,16 @@ class TagViewSet(viewsets.ModelViewSet):
     """
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
+
+    @detail_route(url_path='information-packages')
+    def information_packages(self, request, pk=None):
+        tag = self.get_object()
+        leaves = tag.get_leafnodes(include_self=True).prefetch_related('information_packages')
+        ips = list(set(ip for l in leaves for ip in l.information_packages.all()))
+
+        page = self.paginate_queryset(ips)
+        if page is not None:
+            serializers = InformationPackageSerializer(page, many=True, context={'request': request})
+            return self.get_paginated_response(serializers.data)
+        serializers = InformationPackageSerializer(ips, many=True, context={'request': request})
+        return Response(serializers.data)
