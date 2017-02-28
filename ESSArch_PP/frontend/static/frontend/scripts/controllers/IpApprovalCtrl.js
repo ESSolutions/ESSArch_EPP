d2b8256a0bd8f67f52f90fdd2039df2c6742bebd
petrus <petrus.jonsson@gmail.com>
2017-02-28 16:28:04 +0100

    Add list view population and update

--- ESSArch_PP/frontend/static/frontend/scripts/controllers/IpApprovalCtrl.js
+++ ESSArch_PP/frontend/static/frontend/scripts/controllers/IpApprovalCtrl.js
@@ -1,6 +1,7 @@
-angular.module('myApp').controller('IpApprovalCtrl', function($scope, $controller, $rootScope) {
+angular.module('myApp').controller('IpApprovalCtrl', function($scope, $controller, $rootScope, Resource, $interval, $timeout, appConfig) {
     var vm = this;
     $controller('BaseCtrl', { $scope: $scope });
+    var ipSortString = "";
     //Request form data
     vm.request = {
         type: "preserve_ip",
@@ -10,12 +11,100 @@ angular.module('myApp').controller('IpApprovalCtrl', function($scope, $controlle
             options: ["Disk", "Tape(type1)", "Tape(type2)"]
         }
     };
-    vm.displayedIps = [{
-        Label: "test",
-        Responsible: {username: $rootScope.auth.username},
-        CreateDate: new Date(),
-        ArchivistOrganization: "The test archivist",
-        State: "Test IP",
-        status: 100,
-    }];
+    $scope.$watch(function(){return $rootScope.ipUrl;}, function(newValue, oldValue) {
+        $scope.getListViewData();
+    }, true);
+    /*******************************************/
+    /*Piping and Pagination for List-view table*/
+    /*******************************************/
+
+    var ctrl = this;
+    $scope.selectedIp = {id: "", class: ""};
+    $scope.selectedProfileRow = {profile_type: "", class: ""};
+    this.displayedIps = [];
+    //Get data according to ip table settings and populates ip table
+    this.callServer = function callServer(tableState) {
+        $scope.ipLoading = true;
+        if(vm.displayedIps.length == 0) {
+            $scope.initLoad = true;
+        }
+        if(!angular.isUndefined(tableState)) {
+            $scope.tableState = tableState;
+            var search = "";
+            if(tableState.search.predicateObject) {
+                var search = tableState.search.predicateObject["$"];
+            }
+            var sorting = tableState.sort;
+            var pagination = tableState.pagination;
+            var start = pagination.start || 0;     // This is NOT the page number, but the index of item in the list that you want to use to display the table.
+            var number = pagination.number || vm.itemsPerPage;  // Number of entries showed per page.
+            var pageNumber = start/number+1;
+
+            Resource.getIpPage(start, number, pageNumber, tableState, $scope.selectedIp, sorting, search, ipSortString).then(function (result) {
+                ctrl.displayedIps = result.data;
+                tableState.pagination.numberOfPages = result.numberOfPages;//set the number of pages so the pagination can update
+                $scope.ipLoading = false;
+                $scope.initLoad = false;
+            });
+        }
+    };
+    //Make ip selected and add class to visualize
+    $scope.selectIp = function(row) {
+        vm.displayedIps.forEach(function(ip) {
+            if(ip.id == $scope.selectedIp.id){
+                ip.class = "";
+            }
+        });
+        if(row.id == $scope.selectedIp.id){
+            $scope.selectedIp = {id: "", class: ""};
+        } else {
+            row.class = "selected";
+            $scope.selectedIp = row;
+        }
+    };
+    //Get data for list view
+    $scope.getListViewData = function() {
+        vm.callServer($scope.tableState);
+        $rootScope.loadNavigation(ipSortString);
+        $rootScope.loadTags();
+    };
+    //Update ip list view with an interval
+    //Update only if status < 100 and no step has failed in any IP
+    var listViewInterval;
+    function updateListViewConditional() {
+        $interval.cancel(listViewInterval);
+        listViewInterval = $interval(function() {
+            var updateVar = false;
+            vm.displayedIps.forEach(function(ip, idx) {
+                if(ip.status < 100) {
+                    if(ip.step_state != "FAILURE") {
+                        updateVar = true;
+                    }
+                }
+            });
+            if(updateVar) {
+                $scope.getListViewData();
+            } else {
+                $interval.cancel(listViewInterval);
+                listViewInterval = $interval(function() {
+                    var updateVar = false;
+                    vm.displayedIps.forEach(function(ip, idx) {
+                        if(ip.status < 100) {
+                            if(ip.step_state != "FAILURE") {
+                                updateVar = true;
+                            }
+                        }
+                    });
+                    if(!updateVar) {
+                        $scope.getListViewData();
+                    } else {
+                        updateListViewConditional();
+                    }
+
+                }, appConfig.ipIdleInterval);
+            }
+        }, appConfig.ipInterval);
+    };
+    updateListViewConditional();
+
 });
