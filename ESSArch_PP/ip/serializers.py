71d7a5789309ed337755910e0c89ab8f2941baf9
Oskar Persson <oskar.persson@polken.se>
2017-03-02 14:44:54 +0100

    Add package_type to IP serializer

--- ESSArch_PP/ip/serializers.py
+++ ESSArch_PP/ip/serializers.py
@@ -4,9 +4,11 @@ from ESSArch_Core.ip.models import InformationPackage
 
 
 class InformationPackageSerializer(serializers.HyperlinkedModelSerializer):
+    package_type = serializers.ChoiceField(choices=InformationPackage.PACKAGE_TYPE_CHOICES)
+
     class Meta:
         model = InformationPackage
-        fields = ('url', 'id', 'Label', 'ObjectIdentifierValue',)
+        fields = ('url', 'id', 'Label', 'ObjectIdentifierValue', 'package_type')
 
 
 class InformationPackageDetailSerializer(InformationPackageSerializer):
