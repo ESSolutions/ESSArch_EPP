02a755e132fcb04eb55f7e2476f2660f5a02471d
Oskar Persson <oskar.persson@polken.se>
2017-03-15 11:32:17 +0100

    Fix PEP 8 warnings

--- ESSArch_PP/storage/serializers.py
+++ ESSArch_PP/storage/serializers.py
@@ -6,24 +6,29 @@ from ESSArch_Core.storage.models import (
     StorageTarget,
 )
 
+
 class StorageObjectSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = StorageObject
         fields = (
-                'url', 'id', 'content_location_type', 'content_location_value', 'last_changed_local',
-                'last_changed_external','ip', 'storage_medium'
+            'url', 'id', 'content_location_type', 'content_location_value', 'last_changed_local',
+            'last_changed_external', 'ip', 'storage_medium'
         )
+
+
 class StorageMediumSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = StorageMedium
         fields = (
-                'url', 'id', 'medium_id', 'status', 'location', 'location_status', 'block_size', 'format',
-                'used_capacity', 'number_of_mounts', 'create_date' , 'agent', 'storage_target'
+            'url', 'id', 'medium_id', 'status', 'location', 'location_status', 'block_size', 'format',
+            'used_capacity', 'number_of_mounts', 'create_date', 'agent', 'storage_target'
         )
+
+
 class StorageTargetSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = StorageTarget
         fields = (
-                'url', 'id', 'name', 'status' , 'type', 'default_block_size', 'default_format', 'min_chunk_size',
-                'min_capacity_warning', 'max_capacity', 'remote_server', 'master_server', 'target'
+            'url', 'id', 'name', 'status', 'type', 'default_block_size', 'default_format', 'min_chunk_size',
+            'min_capacity_warning', 'max_capacity', 'remote_server', 'master_server', 'target'
         )
