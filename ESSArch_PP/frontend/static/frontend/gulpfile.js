84762662a605f0dda51c91485cd5b7bde641dce3
petrus <petrus.jonsson@gmail.com>
2017-04-19 17:12:59 +0200

    Add swedish locale for calendar

--- ESSArch_PP/frontend/static/frontend/gulpfile.js
+++ ESSArch_PP/frontend/static/frontend/gulpfile.js
@@ -46,6 +46,7 @@ var jsVendorFiles = [
         'scripts/bower_components/jquery-ui/jquery-ui.js',
         'scripts/bower_components/ua-parser-js/src/ua-parser.js',
         'scripts/bower_components/angular/angular.js',
+        'scripts/angular-locale_sv.js',
         'scripts/bower_components/angular-animate/angular-animate.js',
         'scripts/bower_components/angular-messages/angular-messages.js',
         'scripts/bower_components/angular-route/angular-route.js',
