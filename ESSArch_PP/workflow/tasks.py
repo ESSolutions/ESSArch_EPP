de4a555ea71262357b5648b5b0ff00aabe998df8
Oskar Persson <oskar.persson@polken.se>
2017-04-21 13:20:32 +0200

    Get correct start and end date on receive

--- ESSArch_PP/workflow/tasks.py
+++ ESSArch_PP/workflow/tasks.py
@@ -100,8 +100,8 @@ class ReceiveSIP(DBTask):
             entry_date=parsed.get('create_date'),
             aic=aic,
             Responsible_id=self.responsible,
-            Startdate=parsed['altrecordids'].get('STARTDATE'),
-            Enddate=parsed['altrecordids'].get('ENDDATE'),
+            Startdate=parsed['altrecordids'].get('STARTDATE')[0],
+            Enddate=parsed['altrecordids'].get('ENDDATE')[0],
             information_class=information_class,
         )
 
