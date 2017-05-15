f6f345bed539a5ac22a487cbe44c5dfb60d35620
Oskar Persson <oskar.persson@polken.se>
2017-05-15 16:45:45 +0200

    Require label in PrepareDIP task

--- ESSArch_PP/workflow/tasks.py
+++ ESSArch_PP/workflow/tasks.py
@@ -423,7 +423,7 @@ class AccessAIP(DBTask):
 
 
 class PrepareDIP(DBTask):
-    def run(self, label=None, object_identifier_value=None, orders=[]):
+    def run(self, label, object_identifier_value=None, orders=[]):
         disseminations = Path.objects.get(entity='disseminations').value
 
         ip = InformationPackage.objects.create(
@@ -453,10 +453,10 @@ class PrepareDIP(DBTask):
 
         return ip.pk
 
-    def undo(self, label=None, object_identifier_value=None, orders=[]):
+    def undo(self, label, object_identifier_value=None, orders=[]):
         pass
 
-    def event_outcome_success(self, label=None, object_identifier_value=None, orders=[]):
+    def event_outcome_success(self, label, object_identifier_value=None, orders=[]):
         return 'Prepared DIP "%s"' % self.ip
 
 
