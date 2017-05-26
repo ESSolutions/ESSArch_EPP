060431f94dcfaa9bd15176c8fa959b9af904e33a
Oskar Persson <oskar.persson@polken.se>
2017-05-26 17:34:42 +0200

    Add tests for task viewset

--- ESSArch_PP/workflow/tests/test_views.py
+++ ESSArch_PP/workflow/tests/test_views.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
 from django.contrib.auth.models import Permission, User
 from django.test import TestCase
 from django.urls import reverse
@@ -8,6 +12,55 @@ from rest_framework.test import APIClient
 from ESSArch_Core.WorkflowEngine.models import (
     ProcessStep, ProcessTask,
 )
+
+
+class ProcessTaskViewSetTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username="admin")
+
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    def test_detail_params_with_unicode(self):
+        t = ProcessTask.objects.create(params={'foo': 'åäö'})
+        url = reverse('processtask-detail', args=(t.pk,))
+
+        self.client.get(url)
+
+    def test_filter(self):
+        url = reverse('processtask-list')
+
+        t = ProcessTask.objects.create()
+        t_undo = ProcessTask.objects.create()
+        t_retry = ProcessTask.objects.create()
+
+        t.undone = t_undo
+        t.retried = t_retry
+        t.save(update_fields=['undone', 'retried'])
+
+        res = self.client.get(url)
+        self.assertEqual(len(res.data), 3)
+
+        res = self.client.get(url, {'undo_type': True})
+        self.assertEqual(len(res.data), 1)
+        self.assertEqual(res.data[0]['id'], str(t_undo.pk))
+
+        res = self.client.get(url, {'undo_type': False})
+        self.assertEqual(len(res.data), 2)
+
+        res = self.client.get(url, {'retry_type': True})
+        self.assertEqual(len(res.data), 1)
+        self.assertEqual(res.data[0]['id'], str(t_retry.pk))
+
+        res = self.client.get(url, {'retry_type': False})
+        self.assertEqual(len(res.data), 2)
+
+        res = self.client.get(url, {'undo_type': True, 'retry_type': True})
+        self.assertEqual(len(res.data), 0)
+
+        res = self.client.get(url, {'undo_type': False, 'retry_type': False})
+        self.assertEqual(len(res.data), 1)
+        self.assertEqual(res.data[0]['id'], str(t.pk))
 
 
 class UndoTaskTestCase(TestCase):
