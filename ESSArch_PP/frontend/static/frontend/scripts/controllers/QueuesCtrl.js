ac6e17f5b730c6dfd91fc80817651ecf533bdcaa
petrus <petrus.jonsson@gmail.com>
2017-06-30 11:24:02 +0200

    Remove unused code

--- ESSArch_PP/frontend/static/frontend/scripts/controllers/QueuesCtrl.js
+++ ESSArch_PP/frontend/static/frontend/scripts/controllers/QueuesCtrl.js
@@ -1,7 +1,5 @@
 angular.module('myApp').controller('QueuesCtrl', function($scope, $rootScope, Storage) {
     var vm = this;
-    console.log("Queues page opened");
-    $scope.text = "hello this is queues page";
     $scope.select = true;
     vm.ioQueue = [];
     vm.robotQueue = [];
