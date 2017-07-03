97eb33ca479779f259f801f96c4a5dfcd64615ee
petrus <petrus.jonsson@gmail.com>
2017-07-03 10:26:17 +0200

    Add update interval on queues

--- ESSArch_PP/frontend/static/frontend/scripts/controllers/QueuesCtrl.js
+++ ESSArch_PP/frontend/static/frontend/scripts/controllers/QueuesCtrl.js
@@ -1,8 +1,24 @@
-angular.module('myApp').controller('QueuesCtrl', function($scope, $rootScope, Storage) {
+angular.module('myApp').controller('QueuesCtrl', function(appConfig, $scope, $rootScope, Storage, $interval) {
     var vm = this;
     $scope.select = true;
     vm.ioQueue = [];
     vm.robotQueue = [];
+    var ioInterval;
+    $interval.cancel(ioInterval);
+    ioInterval = $interval(function() {
+        vm.getIoQueue();
+    }, appConfig.queueInterval);
+
+    var robotInterval;
+    $interval.cancel(robotInterval);
+    robotInterval = $interval(function() {
+        vm.getRobotQueue();
+    }, appConfig.queueInterval);
+
+    $rootScope.$on('$stateChangeStart', function() {
+		$interval.cancel(ioInterval);
+		$interval.cancel(robotInterval);
+	});
 
     vm.getIoQueue = function() {
         Storage.getIoQueue().then(function(result) {
