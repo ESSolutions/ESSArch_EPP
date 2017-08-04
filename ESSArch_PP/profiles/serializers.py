929be21354a83f62554ec21ad273a70b7f4842af
Oskar Persson <oskar.persson@polken.se>
2017-08-04 16:57:41 +0200

    Generate base elements on template creation in serializer

--- ESSArch_PP/profiles/serializers.py
+++ ESSArch_PP/profiles/serializers.py
@@ -1,6 +1,11 @@
+from lxml import etree
+
 from rest_framework import serializers
 
+import requests
+
 from ESSArch_Core.essxml.ProfileMaker.models import extensionPackage, templatePackage
+from ESSArch_Core.essxml.ProfileMaker.xsdtojson import generateJsonRes
 
 class ProfileMakerExtensionSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,9 +15,35 @@ class ProfileMakerExtensionSerializer(serializers.ModelSerializer):
         )
 
 class ProfileMakerTemplateSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        schema_request = requests.get(validated_data['schemaURL'])
+        schema_request.raise_for_status()
+
+        schemadoc = etree.fromstring(schema_request.content)
+        targetNamespace = schemadoc.get('targetNamespace')
+        nsmap = {k: v for k, v in schemadoc.nsmap.iteritems() if k and v != "http://www.w3.org/2001/XMLSchema"}
+
+        existingElements, allElements = generateJsonRes(schemadoc, validated_data['root_element'], validated_data['prefix']);
+        existingElements["root"]["nsmap"] = nsmap
+
+        return templatePackage.objects.create(
+            existingElements=existingElements, allElements=allElements,
+            targetNamespace=targetNamespace, **validated_data
+        )
+
     class Meta:
         model = templatePackage
         fields = (
             'existingElements', 'allElements', 'name', 'root_element',
             'extensions', 'prefix', 'schemaURL', 'targetNamespace',
         )
+
+        read_only_fields = (
+            'existingElements', 'allElements', 'extensions',
+        )
+
+        extra_kwargs = {
+            'targetNamespace': {
+                'required': False
+            }
+        }
