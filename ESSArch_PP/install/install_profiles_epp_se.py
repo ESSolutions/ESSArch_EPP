b115940c24fabdb4a677494a31391fb31c6b919b
Oskar Persson <oskar.persson@polken.se>
2017-09-18 09:44:48 +0200

    Add profiles from ETP SE install script

--- ESSArch_PP/install/install_profiles_epp_se.py
+++ ESSArch_PP/install/install_profiles_epp_se.py
@@ -43,6 +43,10 @@ from ESSArch_Core.profiles.models import (
 def installProfiles():
     sa = installSubmissionAgreement()
 
+    installProfileSIP(sa)
+    installProfileTransferProject(sa)
+    installProfileSubmitDescription(sa)
+
     installProfileAIP(sa)
     installProfileDIP(sa)
     installProfilePreservationMetadata(sa)
@@ -108,6 +112,510 @@ def installSubmissionAgreement():
     print 'Installed submission agreement'
 
     return sa
+
+
+def installProfileTransferProject(sa):
+
+    dct = {
+        'name': 'Transfer Project Profile 1',
+        'profile_type': 'transfer_project',
+        'type': 'Implementation',
+        'status': 'Agreed',
+        'label': 'Transfer Project Profile 1',
+        'template': [
+            {
+                "templateOptions": {
+                    "type": "text",
+                    "label": "Archival institution",
+                },
+                "type": "input",
+                "defaultValue": "Riksarkivet",
+                "key": "archival_institution"
+            }, {
+                "templateOptions": {
+                    "type": "text",
+                    "disabled": True,
+                    "label": "Archivist Organization"
+                },
+                "type": "input",
+                "key": "_IP_ARCHIVIST_ORGANIZATION"
+            }, {
+                "templateOptions": {
+                    "type": "text",
+                    "label": "Archival type",
+                },
+                "type": "input",
+                "defaultValue": "document",
+                "key": "archival_type"
+            }, {
+                "templateOptions": {
+                    "type": "text",
+                    "label": "Archival location",
+                },
+                "type": "input",
+                "defaultValue": "sweden-stockholm-nacka",
+                "key": "archival_location"
+            }, {
+                "templateOptions": {
+                    "disabled": True,
+                    "type": "text",
+                    "label": "Archive Policy",
+                },
+                "type": "input",
+                "defaultValue": "archive policy 1",
+                "key": "archive_policy"
+            }, {
+                'key': 'container_format',
+                'type': 'select',
+                'defaultValue': 'tar',
+                'templateOptions': {
+                  'label': 'Container format',
+                  'options': [
+                    {'name': 'TAR', 'value': 'tar'},
+                    {'name': 'ZIP', 'value': 'zip'},
+                  ]
+                }
+            }, {
+                "key": "container_format_compression",
+                "templateOptions": {
+                    "label": "Container format compression",
+                    "options": [
+                        {
+                            "name": "Yes",
+                            "value": True
+                        },
+                        {
+                            "name": "No",
+                            "value": False
+                        }
+                    ]
+                },
+                "defaultValue": False,
+                "type": "select"
+            }, {
+                'key': 'checksum_algorithm',
+                'type': 'select',
+                'defaultValue': 'MD5',
+                'templateOptions': {
+                  'label': 'Checksum algorithm',
+                  'options': [
+                    {'name': 'MD5', 'value': 'MD5'},
+                    {'name': 'SHA-256', 'value': 'SHA-256'},
+                  ]
+                }
+            }, {
+                "templateOptions": {
+                    "type": "email",
+                    "label": "Preservation organization receiver email",
+                },
+                "type": "input",
+                "key": "preservation_organization_receiver_email"
+            }, {
+                "templateOptions": {
+                    "type": "url",
+                    "remote": "",
+                    "label": "Preservation organization receiver url (empty for local)",
+                },
+                "type": "input",
+                "key": "preservation_organization_receiver_url"
+            }
+        ],
+    }
+
+    profile, _ = Profile.objects.update_or_create(name=dct['name'], defaults=dct)
+    ProfileSA.objects.get_or_create(profile=profile, submission_agreement=sa)
+
+    print 'Installed profile transfer project'
+
+    return 0
+
+
+def installProfileSubmitDescription(sa):
+
+    dct = {
+        'name': 'Submit description of a single SIP',
+        'profile_type': 'submit_description',
+        'type': 'Implementation',
+        'status': 'Draft',
+        'label': 'Submit description of a single SIP',
+        'template': [
+            {
+                "templateOptions": {
+                    "type": "text",
+                    "label": "Start Date"
+                },
+                "type": "datepicker",
+                "defaultValue": "2016-11-10",
+                "key": "startdate"
+            },
+            {
+                "templateOptions": {
+                    "type": "text",
+                    "label": "End Date"
+                },
+                "type": "datepicker",
+                "defaultValue": "2016-12-20",
+                "key": "enddate"
+            },
+            {
+                "templateOptions": {
+                    "type": "text",
+                    "disabled": True,
+                    "label": "Archivist Organization"
+                },
+                "type": "input",
+                "key": "_IP_ARCHIVIST_ORGANIZATION"
+            },
+            {
+                "templateOptions": {
+                    "type": "text",
+                    "label": "Creator"
+                },
+                "type": "input",
+                "defaultValue": "the creator",
+                "key": "creator"
+            },
+            {
+                "templateOptions": {
+                    "type": "text",
+                    "label": "Submitter Organization"
+                },
+                "type": "input",
+                "defaultValue": "the submitter organization",
+                "key": "submitter_organization"
+            },
+            {
+                "templateOptions": {
+                    "type": "text",
+                    "label": "Submitter Individual"
+                },
+                "type": "input",
+                "defaultValue": "the submitter individual",
+                "key": "submitter_individual"
+            },
+            {
+                "templateOptions": {
+                    "type": "text",
+                    "label": "Producer Organization"
+                },
+                "type": "input",
+                "defaultValue": "the producer organization",
+                "key": "producer_organization"
+            },
+            {
+                "templateOptions": {
+                    "type": "text",
+                    "label": "Producer Individual"
+                },
+                "type": "input",
+                "defaultValue": "the producer individual",
+                "key": "producer_individual"
+            },
+            {
+                "templateOptions": {
+                    "type": "text",
+                    "label": "IP Owner"
+                },
+                "type": "input",
+                "defaultValue": "the ip owner",
+                "key": "ipowner"
+            },
+            {
+                "templateOptions": {
+                    "type": "text",
+                    "label": "Preservation Organization"
+                },
+                "type": "input",
+                "defaultValue": "the preservation organization",
+                "key": "preservation_organization"
+            },
+            {
+                "templateOptions": {
+                    "type": "text",
+                    "label": "System Name"
+                },
+                "type": "input",
+                "defaultValue": "the system name",
+                "key": "systemname"
+            },
+            {
+                "templateOptions": {
+                    "type": "text",
+                    "label": "System Version"
+                },
+                "type": "input",
+                "defaultValue": "the system version",
+                "key": "systemversion"
+            },
+            {
+                "templateOptions": {
+                    "type": "text",
+                    "label": "System Type"
+                },
+                "type": "input",
+                "defaultValue": "the system type",
+                "key": "systemtype"
+            },
+        ],
+        'specification': json.loads(open(os.path.join(settings.BASE_DIR, 'templates/SE_SD_Template.json')).read()),
+    }
+
+    profile, _ = Profile.objects.update_or_create(name=dct['name'], defaults=dct)
+    ProfileSA.objects.get_or_create(profile=profile, submission_agreement=sa)
+
+    print 'Installed profile submit description'
+
+    return 0
+
+
+def installProfileSIP(sa):
+
+    dct = {
+        'name': 'SIP SE',
+        'profile_type': 'sip',
+        'type': 'Implementation',
+        'status': 'Draft',
+        'label': 'SIP profile for SE submissions',
+        'representation_info': 'Documentation 1',
+        'preservation_descriptive_info': 'Documentation 2',
+        'supplemental': 'Documentation 3',
+        'access_constraints': 'Documentation 4',
+        'datamodel_reference': 'Documentation 5',
+        'additional': 'Documentation 6',
+        'submission_method': 'Electronically',
+        'submission_schedule': 'Once',
+        'submission_data_inventory': 'According to submit description',
+        'structure': [
+            {
+                'type': 'file',
+                'name': 'mets.xml',
+                'use': 'mets_file',
+            },
+            {
+                'type': 'folder',
+                'name': 'content',
+                'children': [
+                    {
+                        'type': 'file',
+                        'name': 'mets_grp',
+                        'use': 'mets_grp',
+                    },
+                    {
+                        'type': 'folder',
+                        'name': 'data',
+                        'children': [],
+                    },
+                    {
+                        'type': 'folder',
+                        'name': 'metadata',
+                        'children': [],
+                    },
+                ]
+            },
+            {
+                'type': 'folder',
+                'name': 'metadata',
+                'children': [
+                    {
+                        'type': 'file',
+                        'use': 'xsd_files',
+                        'name': 'xsd_files'
+                    },
+                    {
+                        'type': 'file',
+                        'name': 'premis.xml',
+                        'use': 'preservation_description_file',
+                    },
+                    {
+                        'type': 'file',
+                        'name': 'ead.xml',
+                        'use': 'archival_description_file',
+                    },
+                    {
+                        'type': 'file',
+                        'name': 'eac.xml',
+                        'use': 'authoritive_information_file',
+                    },
+                ]
+            },
+        ],
+        'template': [
+            {
+                "key": "mets_type",
+                "type": "select",
+                "defaultValue": "Personnel",
+                "templateOptions": {
+                    "label": "Content Type",
+                    "options": [
+                        {
+                          "name": "Electronic Record Management System",
+                          "value": "ERMS"
+                        },
+                        {
+                          "name": "Personnel system",
+                          "value": "Personnel"
+                        },
+                        {
+                          "name": "Medical record(s)",
+                          "value": "Medical record"
+                        },
+                        {
+                          "name": "Economics",
+                          "value": "Economics systems"
+                        },
+                        {
+                          "name": "Databases",
+                          "value": "Databases"
+                        },
+                        {
+                          "name": "Webpages",
+                          "value": "Webpages"
+                        },
+                        {
+                          "name": "Geografical Information Systems",
+                          "value": "GIS"
+                        },
+                        {
+                          "name": "No specification",
+                          "value": "No specification"
+                        },
+                        {
+                          "name": "Archival Information Collection",
+                          "value": "AIC"
+                        },
+                        {
+                          "name": "Archival Information",
+                          "value": "Archival Information"
+                        },
+                        {
+                          "name": "Unstructured",
+                          "value": "Unstructured"
+                        },
+                        {
+                          "name": "Single records",
+                          "value": "Single records"
+                        },
+                        {
+                          "name": "Publication",
+                          "value": "Publication"
+                        },
+                    ]
+                },
+            },
+            {
+                "key": "RECORDSTATUS",
+                "type": "select",
+                "defaultValue": "NEW",
+                "templateOptions": {
+                    "label": "Record Status",
+                    "options": [
+                        {
+                          "name": "SUPPLEMENT",
+                          "value": "SUPPLEMENT"
+                        },
+                        {
+                          "name": "REPLACEMENT",
+                          "value": "REPLACEMENT"
+                        },
+                        {
+                            "name": "NEW",
+                            "value": "NEW"
+                        },
+                        {
+                          "name": "TEST",
+                          "value": "TEST"
+                        },
+                        {
+                          "name": "VERSION",
+                          "value": "VERSION"
+                        },
+                        {
+                          "name": "OTHER",
+                          "value": "OTHER"
+                        },
+                    ]
+                },
+            },
+            {
+                "templateOptions": {
+                    "type": "text",
+                    "disabled": True,
+                    "label": "Archivist Organization"
+                },
+                "type": "input",
+                "key": "_IP_ARCHIVIST_ORGANIZATION"
+            },
+            {
+                "templateOptions": {
+                    "type": "text",
+                    "label": "Archivist Organization Note"
+                },
+                "type": "input",
+                "defaultValue": "Archivist Organization 1 Note",
+                "key": "archivist_organization_note"
+            },
+            {
+                "templateOptions": {
+                    "type": "text",
+                    "label": "Creator Organization",
+                    "required": True,
+                },
+                "type": "input",
+                "defaultValue": "Creator Organization 1",
+                "key": "creator_organization_name"
+            },
+            {
+                "templateOptions": {
+                    "type": "text",
+                    "label": "Creator Organization Note"
+                },
+                "type": "input",
+                "defaultValue": "Creator Organization 1 Note",
+                "key": "creator_organization_note"
+            },
+            {
+                "templateOptions": {
+                    "type": "text",
+                    "label": "Archivist Software",
+                    "required": True,
+                },
+                "type": "input",
+                "defaultValue": "Archivist Software 1",
+                "key": "archivist_software_name"
+            },
+            {
+                "templateOptions": {
+                    "type": "text",
+                    "label": "Archivist Software Note"
+                },
+                "type": "input",
+                "defaultValue": "Archivist Software 1 Note",
+                "key": "archivist_software_note"
+            },
+            {
+                "templateOptions": {
+                    "label": "Allow unknown file types",
+                    "options": [
+                        {"name": "Yes", "value": True},
+                        {"name": "No", "value": False},
+                    ],
+                },
+                "type": "select",
+                "defaultValue": True,
+                "key": "allow_unknown_file_types"
+            },
+        ],
+        'specification': json.loads(open(os.path.join(settings.BASE_DIR, 'templates/SE_SIP_Template.json')).read()),
+    }
+
+    profile, _ = Profile.objects.update_or_create(name=dct['name'], defaults=dct)
+    ProfileSA.objects.get_or_create(profile=profile, submission_agreement=sa)
+
+    print 'Installed profile SIP'
+
+    return 0
+
+
 
 
 def installProfileAIP(sa):
