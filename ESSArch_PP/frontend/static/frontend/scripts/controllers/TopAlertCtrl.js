d8ffc334098339c19f20a70e704bd8ce691cc831
petrus <petrus.jonsson@gmail.com>
2017-09-18 15:52:41 +0200

    Update top alert

--- ESSArch_PP/frontend/static/frontend/scripts/controllers/TopAlertCtrl.js
+++ ESSArch_PP/frontend/static/frontend/scripts/controllers/TopAlertCtrl.js
@@ -1,43 +1,109 @@
-angular.module('myApp').controller('TopAlertCtrl', function($timeout, $scope, $rootScope) {
+angular.module('myApp').controller('TopAlertCtrl', function(appConfig, TopAlert, $timeout, $interval, $scope, $rootScope, $http) {
     var vm = this;
     vm.visible = false;
     vm.alerts = [];
+    vm.frontendAlerts = [];
+    vm.backendAlerts = [];
     vm.showAlerts = false;
+    var updateInterval;
+    vm.$onInit = function() {
+        $interval.cancel(updateInterval);
+        updateInterval = $interval(function() {
+            TopAlert.getNotifications().then(function(data) {
+                vm.backendAlerts = data;
+                vm.alerts = vm.frontendAlerts.concat(vm.backendAlerts).sort(function(a, b) {
+                    return new Date(b.time_created) - new Date(a.time_created);
+                })
+                if(data.length > 0 && !data[0].seen) {
+                    vm.showAlert();
+                }
+            });
+        }, appConfig.notificationInterval);
+    }
+
+    $rootScope.getUnseen = function() {
+        var unseen = 0;
+        vm.alerts.forEach(function(alert) {
+            if(!alert.seen) {
+                unseen += 1;
+            }
+        })
+        return unseen;
+    }
+
+    vm.showAlert = function() {
+        vm.visible = true;
+        vm.setSeen([vm.alerts[0]]);
+    }
+
+    vm.setSeen = function(alerts) {
+        alerts.forEach(function(alert) {
+            if(alert.id && !alert.seen) {
+                $http({
+                    method: 'PATCH',
+                    url: appConfig.djangoUrl + 'notifications/' + alert.id + '/',
+                    data: { seen: true }
+                });
+            } else if(!alert.id) {
+                alert.seen = true;
+            }
+        });
+    }
 
     vm.hideAlert = function() {
         vm.visible = false;
         vm.showAlerts = false;
-        vm.alerts.splice(0, 1);
+    }
+    vm.removeAndHideAlert = function(alert) {
+        vm.removeAlert(alert);
+        vm.visible = false;
+        vm.showAlerts = false;
     }
 
-    vm.removeAlert = function(alert) {
-        vm.alerts.splice(vm.alerts.indexOf(alert), 1);
-        if(vm.alerts.length == 0) {
-            vm.showAlerts = false;
+    vm.removeAlert = function (alert) {
+        if (alert.id) {
+            TopAlert.delete(alert.id).then(function (response) {
+                vm.backendAlerts.splice(vm.backendAlerts.indexOf(alert), 1);
+                vm.alerts.splice(vm.alerts.indexOf(alert), 1);
+                if (vm.alerts.length == 0) {
+                    vm.showAlerts = false;
+                } else {
+                    vm.setSeen(vm.alerts.slice(0,5))
+                }
+            });
+        } else {
+            vm.frontendAlerts.splice(vm.frontendAlerts.indexOf(alert), 1);
+            vm.alerts.splice(vm.alerts.indexOf(alert), 1);
+            if (vm.alerts.length == 0) {
+                vm.showAlerts = false;
+            }
         }
     }
 
-    vm.viewAlerts = function() {
+    vm.toggleAlerts = function() {
         if(vm.alerts.length > 0) {
             vm.showAlerts = !vm.showAlerts;
+        }
+        if(vm.showAlerts) {
+            vm.setSeen(vm.alerts.slice(0, 5));
         }
     }
 
     /**
      * Show top alert
-     * @param msg - Message to show on the the alert
-     * @param type - Type of alert, applies a class to the alert
+     * @param message - Message to show on the the alert
+     * @param level - level of alert, applies a class to the alert
      * @param time - Adds a duration to the alert
      */
 
-    vm.showAlert = function (msg, type, time) {
-        vm.visible = true;
+    vm.addAlert = function (message, level, time) {
         var timer = null;
-        var alert = {msg: msg, type: type};
-        vm.alerts.unshift(alert);            
+        var alert = {message: message, level: level, time_created: new Date(), seen: true};
+        vm.frontendAlerts.unshift(alert);
         if (time) {
             timer = vm.setTimer(alert, time);
         }
+        vm.showAlert();
     }
 
     /**
@@ -47,39 +113,60 @@ angular.module('myApp').controller('TopAlertCtrl', function($timeout, $scope, $r
     vm.setTimer = function (alert, time) {
         return $timeout(function () {
             vm.removeAlert(alert);
+            vm.setSeen(vm.alerts.slice(0, 5));
         }, time)
     }
 
     // Listen for show/hide events
-    $rootScope.$on('show_top_alert', function(event, data) {
-        vm.showAlert(data.msg, data.type, data.time);
+    $rootScope.$on('add_top_alert', function (event, data) {
+        vm.addAlert(data.message, data.level, data.time);
     });
-    $rootScope.$on('hide_top_alert', function(event, data) {
+    $rootScope.$on('show_top_alert', function (event, data) {
+        vm.showAlert();
+        $timeout(function() {
+            vm.showAlerts = true;
+        }, 300);
+    });
+    $rootScope.$on('hide_top_alert', function (event, data) {
         vm.hideAlert();
     });
 
-}).factory('TopAlert', function($rootScope) {
+}).factory('TopAlert', function ($rootScope, appConfig, $http) {
     return {
         /**
-         * Show top alert
-         * @param msg - Message to show on the the alert
-         * @param type - Type of alert, applies a class to the alert
+         * Add top alert and show it
+         * @param message - Message to show on the the alert
+         * @param level - level of alert, applies a class to the alert
          * @param time - Adds a duration to the alert
          */
-        show: function(msg, type, time) {
-            $rootScope.$broadcast('show_top_alert', { msg: msg, type: type, time: time });
-            
+        add: function(message, level, time) {
+            $rootScope.$broadcast('add_top_alert', { message: message, level: level, time: time });
+        },
+        /**
+         * Show alert
+         */
+        show: function() {
+            $rootScope.$broadcast('show_top_alert', {});
         },
         /**
          * Hide top alert
          */
         hide: function() {
             $rootScope.$broadcast('hide_top_alert', {});
-            
+
+        },
+        getNotifications: function() {
+            return $http.get(appConfig.djangoUrl + "notifications/", {params: {pager: "none"}}).then(function(response) {
+                return response.data;
+            })
+        },
+        delete: function(id) {
+            return $http.delete(appConfig.djangoUrl + "notifications/" + id + "/").then(function(response) {
+                return response;
+            })
+        },
+        getUnseen: function() {
+            return $rootScope.getUnseen();
         }
     }
-}).filter('reverse', function() {
-    return function(items) {
-      return items.slice().reverse();
-    };
 });
