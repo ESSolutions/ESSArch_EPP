6c18b927c41edb5229102768359f96f00c3db76b
petrus <petrus.jonsson@gmail.com>
2017-09-28 16:28:32 +0200

    Use WebSocket to get new alerts etc

--- ESSArch_PP/frontend/static/frontend/scripts/controllers/TopAlertCtrl.js
+++ ESSArch_PP/frontend/static/frontend/scripts/controllers/TopAlertCtrl.js
@@ -6,15 +6,13 @@ angular.module('myApp').controller('TopAlertCtrl', function(appConfig, TopAlert,
     vm.backendAlerts = [];
     vm.showAlerts = false;
     var updateInterval;
-    vm.$onInit = function() {
-        $interval.cancel(updateInterval);
-        updateInterval = $interval(function() {
-            vm.getNotifications();
-            vm.updateUnseen();
-        }, appConfig.notificationInterval);
+    vm.$onInit = function () {
+        vm.getNotifications();
+        vm.updateUnseen();
     }
+
     vm.getNotifications = function() {
-        TopAlert.getNotifications().then(function(data) {
+        return TopAlert.getNotifications().then(function(data) {
             vm.backendAlerts = data;
             vm.alerts = vm.frontendAlerts.concat(vm.backendAlerts).sort(function(a, b) {
                 return new Date(b.time_created) - new Date(a.time_created);
@@ -22,18 +20,25 @@ angular.module('myApp').controller('TopAlertCtrl', function(appConfig, TopAlert,
             if(vm.alerts.length > 0 && !vm.alerts[0].seen) {
                 vm.showAlert();
             }
+            return vm.alerts;
         });
     }
 
-    vm.updateUnseen = function() {
-        $http.head(appConfig.djangoUrl + "notifications/", {params: { seen: false }}).then(function(response) {
-            $rootScope.unseenNotifications = response.headers().count;
-        });
+    vm.updateUnseen = function(count) {
+        if(count) {
+            $rootScope.unseenNotifications = count;
+        } else {
+            $http.head(appConfig.djangoUrl + "notifications/", {params: { seen: false }}).then(function(response) {
+                $rootScope.unseenNotifications = response.headers().count;
+            });
+        }
     }
 
     vm.showAlert = function() {
-        vm.visible = true;
-        vm.setSeen([vm.alerts[0]]);
+        if(vm.alerts.length >0) {
+            vm.visible = true;
+            vm.setSeen([vm.alerts[0]]);
+        }
     }
 
     vm.setSeen = function(alerts) {
@@ -43,7 +48,10 @@ angular.module('myApp').controller('TopAlertCtrl', function(appConfig, TopAlert,
                     method: 'PATCH',
                     url: appConfig.djangoUrl + 'notifications/' + alert.id + '/',
                     data: { seen: true }
-                });
+                }).then(function(response) {
+                    alert.seen = true;
+                    $rootScope.unseenNotifications -= 1;
+                }).catch(function(response) {});
             } else if(!alert.id) {
                 alert.seen = true;
             }
@@ -63,6 +71,9 @@ angular.module('myApp').controller('TopAlertCtrl', function(appConfig, TopAlert,
                 if (vm.alerts.length == 0) {
                     vm.showAlerts = false;
                 } else {
+                if(vm.alerts.length < 7) {
+                    getNext();
+                }
                     vm.setSeen(vm.alerts.slice(0,5))
                 }
             });
@@ -71,16 +82,38 @@ angular.module('myApp').controller('TopAlertCtrl', function(appConfig, TopAlert,
             vm.alerts.splice(vm.alerts.indexOf(alert), 1);
             if (vm.alerts.length == 0) {
                 vm.showAlerts = false;
+            } else {
+                if(vm.alerts.length < 7) {
+                    getNext();
+                }
+                vm.setSeen(vm.alerts.slice(0,5))
             }
-        }
-        if(!vm.showAlerts) {
-            vm.visible = false;
         }
     }
 
+    function getNext() {
+        return TopAlert.getNextNotification().then(function(data) {
+            vm.backendAlerts.push(data[0]);
+            vm.alerts = vm.frontendAlerts.concat(vm.backendAlerts).sort(function(a, b) {
+                return new Date(b.time_created) - new Date(a.time_created);
+            });
+            if(!vm.showAlerts) {
+                vm.visible = false;
+            }
+            return vm.alerts;
+        }).catch(function(response) {
+            if(!vm.showAlerts) {
+                vm.visible = false;
+            }
+            return vm.alerts;
+        })
+    }
     vm.clearAll = function() {
         TopAlert.deleteAll().then(function(response) {
             vm.visible = false;
+            vm.alerts = [];
+            vm.backendAlerts = [];
+            vm.frontendAlerts = [];
         })
     }
 
@@ -100,14 +133,23 @@ angular.module('myApp').controller('TopAlertCtrl', function(appConfig, TopAlert,
      * @param time - Adds a duration to the alert
      */
 
-    vm.addAlert = function (message, level, time) {
+    vm.addAlert = function (id, message, level, time, seen) {
         var timer = null;
         var alert = {message: message, level: level, time_created: new Date(), seen: false};
-        vm.frontendAlerts.unshift(alert);
+        if(id) {
+            alert.id = id;
+            alert.seen = seen
+            vm.backendAlerts.unshift(alert);
+        } else {
+            vm.frontendAlerts.unshift(alert);
+        }
         if (time) {
             timer = vm.setTimer(alert, time);
         }
-        vm.getNotifications();
+        vm.alerts = vm.frontendAlerts.concat(vm.backendAlerts).sort(function(a, b) {
+            return new Date(b.time_created) - new Date(a.time_created);
+        });
+        vm.showAlert();
     }
 
     /**
@@ -125,7 +167,11 @@ angular.module('myApp').controller('TopAlertCtrl', function(appConfig, TopAlert,
 
     // Listen for show/hide events
     $rootScope.$on('add_top_alert', function (event, data) {
-        vm.addAlert(data.message, data.level, data.time);
+        vm.addAlert(data.id, data.message, data.level, data.time, data.seen);
+    });
+    $rootScope.$on('add_unseen_top_alert', function (event, data) {
+        vm.updateUnseen(data.unseen_count);
+        vm.addAlert(data.id, data.message, data.level, data.time, false);
     });
     $rootScope.$on('show_top_alert', function (event, data) {
         vm.showAlert();
@@ -137,9 +183,49 @@ angular.module('myApp').controller('TopAlertCtrl', function(appConfig, TopAlert,
     $rootScope.$on('hide_top_alert', function (event, data) {
         vm.hideAlert();
     });
+    $rootScope.$on('get_top_alerts', function (event, data) {
+        vm.getNotifications();
+    });
 
-}).factory('TopAlert', function ($rootScope, appConfig, $http) {
-    return {
+}).factory('TopAlert', function ($rootScope, $q, appConfig, $http) {
+    // Keep all pending requests here until they get responses
+    var callbacks = {};
+    // Create a unique callback ID to map requests to responses
+    var currentCallbackId = 0;
+    // Create our websocket object with the address to the websocket
+    var ws = new WebSocket("ws://localhost:8002/notifications/");
+    ws.onopen = function () {
+    }
+
+    ws.onmessage = function (message) {
+        listener(message.data);
+    }
+
+    function listener(data) {
+        var messageObj = JSON.parse(data);
+        $rootScope.$broadcast('add_unseen_top_alert', {
+            id: messageObj.id,
+            message: messageObj.message,
+            level: messageObj.level,
+            count: messageObj.unseen_count
+        });
+        // If an object exists with callback_id in our callbacks object, resolve it
+        if (callbacks.hasOwnProperty(messageObj.callback_id)) {
+            $rootScope.$apply(callbacks[messageObj.callback_id].cb.resolve(messageObj.data));
+            delete callbacks[messageObj.callbackID];
+        }
+    }
+
+    // This creates a new callback ID for a request
+    function getCallbackId() {
+        currentCallbackId += 1;
+        if (currentCallbackId > 10000) {
+            currentCallbackId = 0;
+        }
+        return currentCallbackId;
+    }
+
+    var service = {
         /**
          * Add top alert and show it
          * @param message - Message to show on the the alert
@@ -163,7 +249,17 @@ angular.module('myApp').controller('TopAlertCtrl', function(appConfig, TopAlert,
 
         },
         getNotifications: function() {
-            return $http.get(appConfig.djangoUrl + "notifications/", {params: {page_size: 5}}).then(function(response) {
+            return $http.get(appConfig.djangoUrl + "notifications/", {params: {page_size: 7}}).then(function(response) {
+                return response.data;
+            })
+        },
+        getNextNotification: function() {
+            return $http.get(appConfig.djangoUrl + "notifications/", {params: {page_size: 1, page: 7}}).then(function(response) {
+                return response.data;
+            })
+        },
+        getUnseenNotifications: function(date) {
+            return $http.get(appConfig.djangoUrl + "notifications/", {params: {create_date: date}}).then(function(response) {
                 return response.data;
             })
         },
@@ -178,4 +274,5 @@ angular.module('myApp').controller('TopAlertCtrl', function(appConfig, TopAlert,
             })
         }
     }
+    return service;
 });
