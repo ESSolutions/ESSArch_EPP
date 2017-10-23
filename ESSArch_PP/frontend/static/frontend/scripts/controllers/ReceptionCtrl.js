ce679e60ee950305eec1c7067560cf14946eeb3e
Oskar Persson <oskar.persson@polken.se>
2017-10-23 22:09:38 +0200

    Get start and end date directly from IP in reception

--- ESSArch_PP/frontend/static/frontend/scripts/controllers/ReceptionCtrl.js
+++ ESSArch_PP/frontend/static/frontend/scripts/controllers/ReceptionCtrl.js
@@ -290,24 +290,9 @@ angular.module('myApp').controller('ReceptionCtrl', function (TopAlert, IPRecept
     ];
 
     $scope.buildSdForm = function(ip) {
-        var startDate, endDate;
-        try {
-            startDate = [ip.altrecordids.STARTDATE][0];
-        }
-        catch(err) {
-            console.log("FAIL", err)
-            startDate = "";
-        }
-        try {
-            endDate = ip.altrecordids.STARTDATE[0]
-        }
-        catch(err) {
-            console.log("FAIL", err)
-            endDate = "";
-        }
         vm.sdModel = {
-            "start_date": startDate,
-            "end_date": endDate,
+            "start_date": ip.start_date,
+            "end_date": ip.end_date,
             "archivist_organization": ip.archivist_organization.name,
             "creator": ip.creator_organization,
             "submitter_organization": ip.submitter_organization,
