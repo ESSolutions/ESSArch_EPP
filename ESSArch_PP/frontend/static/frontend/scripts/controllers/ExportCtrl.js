9e53128210d0c810ad27508e06f440690f79169a
petrus <petrus.jonsson@gmail.com>
2017-11-29 15:23:04 +0100

    Show only exported SA's in select

--- ESSArch_PP/frontend/static/frontend/scripts/controllers/ExportCtrl.js
+++ ESSArch_PP/frontend/static/frontend/scripts/controllers/ExportCtrl.js
@@ -1,13 +1,13 @@
 angular.module('myApp').controller("ExportCtrl", function ($scope, appConfig, $http, TopAlert, $translate) {
     var vm = this;
     vm.$onInit = function() {
-        $http.get(appConfig.djangoUrl + "submission-agreements/", { params: { pager: "none" } })
-        .then(function (response) {
-            vm.sas = response.data;
-            vm.sa = null;
-        }).catch(function(response) {
-            TopAlert.add(respnse.data.detail, "error");
-        });
+        $http.get(appConfig.djangoUrl + "submission-agreements/", { params: { pager: "none", published: true } })
+            .then(function (response) {
+                vm.sas = response.data;
+                vm.sa = null;
+            }).catch(function(response) {
+                TopAlert.add(response.data.detail, "error");
+            });
 
         $http.get(appConfig.djangoUrl+"profiles/", {params: { pager: "none"}})
             .then(function(response) {
