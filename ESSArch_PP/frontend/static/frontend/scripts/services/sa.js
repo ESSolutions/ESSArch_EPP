b5e66d4885b0ca30a4c00d363aac1d066e29a2e4
petrus <petrus.jonsson@gmail.com>
2017-12-01 14:40:20 +0100

    Remove default parameter from Profile

--- ESSArch_PP/frontend/static/frontend/scripts/services/sa.js
+++ ESSArch_PP/frontend/static/frontend/scripts/services/sa.js
@@ -1,5 +1,5 @@
 angular.module('myApp').factory('SA', function ($resource, appConfig) {
-    return $resource(appConfig.djangoUrl + 'submission-agreements/:id/:action/', { id: "@id" }, {
+    return $resource(appConfig.djangoUrl + 'submission-agreements/:id/:action/', {}, {
         get: {
             method: "GET",
             params: { id: "@id" }
