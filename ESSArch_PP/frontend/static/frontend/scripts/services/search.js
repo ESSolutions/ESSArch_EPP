db3742523d51d1d53935c5813728d86b3a946cc7
petrus <petrus.jonsson@gmail.com>
2018-01-12 14:19:48 +0100

    Update field names and ad translations

--- ESSArch_PP/frontend/static/frontend/scripts/services/search.js
+++ ESSArch_PP/frontend/static/frontend/scripts/services/search.js
@@ -1,4 +1,4 @@
-angular.module('myApp').factory('Search', function($http, $sce, appConfig) {
+angular.module('myApp').factory('Search', function($http, $sce, appConfig, $translate) {
     var service = {};
     var auth = window.btoa("user:user");
     var headers = { "Authorization": "Basic " + auth };
@@ -17,9 +17,11 @@ angular.module('myApp').factory('Search', function($http, $sce, appConfig) {
             var returnData = response.data.hits.map(function (item) {
                 item._source.id = item._id;
                 item._source.name = item._source.name;
-                item._source.text = item._source.reference_code + " - " + item._source.name;
+                item._source.text = item._source.reference_code + " - " + item._source.title;
                 item._source.parent = "#";
-                item._source.type = item._type;
+                if(item._index == "archive") {
+                    item._source.type = $translate.instant("archive");
+                }
                 for (var key in item.highlight) {
                     item._source[key] = $sce.trustAsHtml(item.highlight[key][0]);
                 }
