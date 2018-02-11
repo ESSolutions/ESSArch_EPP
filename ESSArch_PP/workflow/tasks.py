920bfe5c42c1f5550a8dc0f33110088da221f0a0
Oskar Persson <oskar.persson@polken.se>
2018-02-11 22:05:21 +0100

    Send refresh parameter with workarea notification

--- ESSArch_PP/workflow/tasks.py
+++ ESSArch_PP/workflow/tasks.py
@@ -491,7 +491,7 @@ class AccessAIP(DBTask):
             ).run().get()
 
             workarea_obj = Workarea.objects.create(ip=new_aip, user_id=self.responsible, type=Workarea.INGEST, read_only=not new)
-            Notification.objects.create(message="%s is now in workarea" % new_aip.object_identifier_value, level=logging.INFO, user_id=self.responsible)
+            Notification.objects.create(message="%s is now in workarea" % new_aip.object_identifier_value, level=logging.INFO, user_id=self.responsible, refresh=True)
 
             if new:
                 new_aip.object_path = dst_dir
@@ -669,7 +669,7 @@ class PollAccessQueue(DBTask):
             os.remove(dst_tar)
 
         Workarea.objects.create(ip=entry.new_ip, user=entry.user, type=Workarea.ACCESS, read_only=not entry.new)
-        Notification.objects.create(message="%s is now in workarea" % entry.new_ip.object_identifier_value, level=logging.INFO, user=entry.user)
+        Notification.objects.create(message="%s is now in workarea" % entry.new_ip.object_identifier_value, level=logging.INFO, user=entry.user, refresh=True)
 
         if entry.new:
             entry.new_ip.object_path = dst_dir
