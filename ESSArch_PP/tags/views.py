036ab3099d88805ba41f18111642ae5cd7535980
Oskar Persson <oskar.persson@polken.se>
2018-04-16 23:14:54 +0200

    Use Tag model in TagViewSet

--- ESSArch_PP/tags/views.py
+++ ESSArch_PP/tags/views.py
@@ -1,38 +1,29 @@
 from django.db.models import Q
-
 from django_filters.rest_framework import DjangoFilterBackend
-
 from rest_framework import viewsets
-from rest_framework.decorators import detail_route
-from rest_framework.response import Response
-
 from rest_framework_extensions.mixins import NestedViewSetMixin
 
-from ESSArch_Core.ip.models import InformationPackage
-from ESSArch_Core.tags.models import TagVersion
-from ESSArch_Core.tags.serializers import TagVersionSerializerWithoutSource
-
-from ip.serializers import InformationPackageSerializer
+from ESSArch_Core.tags.filters import TagFilter
+from ESSArch_Core.tags.models import Tag, TagStructure
+from ESSArch_Core.tags.serializers import TagSerializer, TagStructureSerializer
 from ip.views import InformationPackageViewSet
-from tags.filters import TagFilter
 
 
 class TagViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
-    """
-    API endpoint that allows tags to be viewed or edited.
-    """
-    queryset = TagVersion.objects.all()
-    serializer_class = TagVersionSerializerWithoutSource
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
 
     filter_backends = (DjangoFilterBackend,)
     filter_class = TagFilter
+
+    http_method_names = ('get', 'head', 'options')
 
     def get_queryset(self):
         qs = self.queryset
         ancestor = self.kwargs.get('parent_lookup_tag')
 
         if ancestor is not None:
-            ancestor = TagVersion.objects.get(pk=ancestor)
+            ancestor = Tag.objects.get(pk=ancestor)
             structure = self.request.query_params.get('structure')
             qs = ancestor.get_descendants(structure)
 
