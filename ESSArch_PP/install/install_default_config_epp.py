4271b85177502834abef8acfddbcc6848cfc2d06
Oskar Persson <oskar.persson@polken.se>
2018-04-26 00:56:09 +0200

    Change checksum algorithm in default policy to MD5

--- ESSArch_PP/install/install_default_config_epp.py
+++ ESSArch_PP/install/install_default_config_epp.py
@@ -335,6 +335,7 @@ def installDefaultArchivePolicies():
     ArchivePolicy.objects.update_or_create(
         policy_id='1',
         defaults={
+            'checksum_algorithm': ArchivePolicy.MD5,
             'policy_name': 'default',
             'cache_storage': cache, 'ingest_path': ingest,
             'receive_extract_sip': True
