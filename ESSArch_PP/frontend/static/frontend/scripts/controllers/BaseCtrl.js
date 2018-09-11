5149a617e9c90283654d2fd6846664ebfa4e752a
petrus <petrus.jonsson@gmail.com>
2018-09-11 17:19:07 +0200

    Move state tree view functionality to core

--- ESSArch_PP/frontend/static/frontend/scripts/controllers/BaseCtrl.js
+++ ESSArch_PP/frontend/static/frontend/scripts/controllers/BaseCtrl.js
@@ -71,7 +71,6 @@ angular.module('myApp').controller('BaseCtrl',  function(IP, Task, Step, vm, ipS
 
     //Cancel update intervals on state change
     $scope.$on('$stateChangeStart', function() {
-        $interval.cancel(stateInterval);
         $interval.cancel(listViewInterval);
         watchers.forEach(function(watcher) {
             watcher();
@@ -81,16 +80,6 @@ angular.module('myApp').controller('BaseCtrl',  function(IP, Task, Step, vm, ipS
     $scope.$on('REFRESH_LIST_VIEW', function (event, data) {
         $scope.getListViewData();
     });
-    var stateInterval;
-    //If status view is visible, start update interval
-    watchers.push($scope.$watch(function(){return $scope.statusShow;}, function(newValue, oldValue) {
-        if(newValue) {
-            $interval.cancel(stateInterval);
-            stateInterval = $interval(function(){$scope.statusViewUpdate($scope.ip)}, appConfig.stateInterval);
-        } else {
-            $interval.cancel(stateInterval);
-        }
-    }));
 
     // list view update interval
 
@@ -148,14 +137,12 @@ angular.module('myApp').controller('BaseCtrl',  function(IP, Task, Step, vm, ipS
             } else {
                 $scope.statusShow = true;
                 $scope.edit = false;
-                $scope.statusViewUpdate(row);
                 $scope.ip = row;
                 $rootScope.ip = row;
             }
         } else {
             $scope.statusShow = true;
             $scope.edit = false;
-            $scope.statusViewUpdate(row);
             $scope.ip = row;
             $rootScope.ip = row;
         }
@@ -434,45 +421,6 @@ angular.module('myApp').controller('BaseCtrl',  function(IP, Task, Step, vm, ipS
         }
     }
 
-    /**
-     * Handle keydown events for state view table
-     * @param {Event} e
-     */
-    $scope.myTreeControl.scope.stateTableKeydownListener = function(e, branch) {
-        switch(e.keyCode) {
-            case arrowDown:
-                e.preventDefault();
-                e.target.nextElementSibling.focus();
-                break;
-            case arrowUp:
-                e.preventDefault();
-                e.target.previousElementSibling.focus();
-                break;
-            case enter:
-                e.preventDefault();
-                $scope.stepTaskClick(branch)
-                break;
-            case space:
-                e.preventDefault();
-                if(branch.flow_type != "task") {
-                    if(branch.expanded) {
-                        branch.expanded = false;
-                    } else {
-                        branch.expanded = true;
-                        $scope.statusViewUpdate($scope.ip);
-                    }
-                }
-                break;
-            case escape:
-                e.preventDefault();
-                if($scope.ip) {
-                    closeContentViews();
-                }
-                document.getElementById("list-view").focus();
-                break;
-        }
-    }
-
     // Validators
 
     vm.validatorModel = {
@@ -736,149 +684,6 @@ angular.module('myApp').controller('BaseCtrl',  function(IP, Task, Step, vm, ipS
 
     $scope.menuOptions = [];
 
-    //Status tree view structure
-    $scope.tree_data = [];
-    $scope.angular = angular;
-    $scope.statusShow = false;
-    $scope.eventShow = false;
-    $translate(['LABEL', 'RESPONSIBLE', 'DATE', 'STATE', 'STATUS']).then(function(translations) {
-        $scope.responsible = translations.RESPONSIBLE;
-        $scope.label = translations.LABEL;
-        $scope.date = translations.DATE;
-        $scope.state = translations.STATE;
-        $scope.status = translations.STATUS;
-        $scope.expanding_property = {
-            field: "label",
-            displayName: $scope.label,
-        };
-        $scope.col_defs = [
-            {
-                field: "responsible",
-                displayName: $scope.responsible
-            },
-            {
-                cellTemplate: "<div ng-include src=\"'static/frontend/views/task_pagination.html'\"></div>"
-            },
-            {
-                field: "time_started",
-                displayName: $scope.date
-
-            },
-            {
-                field: "status",
-                displayName: $scope.state,
-                cellTemplate: "<div ng-if=\"row.branch[col.field] == 'SUCCESS'\" class=\"step-state-success\"><b>{{'SUCCESS' | translate}}</b></div><div ng-if=\"row.branch[col.field] == 'FAILURE'\" class=\"step-state-failure\"><b>{{'FAILURE' | translate}}</b></div><div ng-if=\"row.branch[col.field] != 'SUCCESS' && row.branch[col.field] !='FAILURE'\" class=\"step-state-in-progress\"><b>{{'INPROGRESS' | translate}}</b></div>"
-
-            },
-            {
-                field: "progress",
-                displayName: $scope.status,
-                cellTemplate: "<uib-progressbar class=\"progress\" value=\"row.branch[col.field]\" type=\"success\"><b>{{row.branch[col.field]+\"%\"}}</b></uib-progressbar>"
-            }
-        ];
-        if($scope.checkPermission("WorkflowEngine.can_undo")) {
-            $scope.col_defs.push({cellTemplate: "<div ng-include src=\"'workflow/undo.html'\"></div>"});
-        }
-        if($scope.checkPermission("WorkflowEngine.can_retry")) {
-            $scope.col_defs.push({cellTemplate: "<div ng-include src=\"'workflow/redo.html'\"></div>"});
-        }
-    });
-
-    //Undo step/task
-    $scope.myTreeControl.scope.taskStepUndo = function(branch) {
-        branch.$undo().then(function(response) {
-            $timeout(function(){
-                $scope.statusViewUpdate($scope.ip);
-            }, 1000);
-        }).catch(function() {
-            console.log("error");
-        });
-    };
-    //Redo step/task
-    $scope.myTreeControl.scope.taskStepRedo = function(branch){
-        branch.$retry().then(function(response) {
-            $timeout(function(){
-                $scope.statusViewUpdate($scope.ip);
-            }, 1000);
-        }).catch(function() {
-            console.log("error");
-        });
-    };
-    $scope.currentStepTask = {id: ""}
-    $scope.myTreeControl.scope.updatePageNumber = function(branch, page) {
-        if(page > branch.page_number && branch.next){
-            branch.page_number = parseInt(branch.next.page);
-            listViewService.getChildrenForStep(branch, branch.page_number).then(function(result) {
-                branch = result;
-            })
-        } else if(page < branch.page_number && branch.prev && page > 0) {
-            branch.page_number = parseInt(branch.prev.page);
-            listViewService.getChildrenForStep(branch, branch.page_number).then(function(result) {
-                branch = result;
-            })
-        }
-    };
-
-    //Click on +/- on step
-    $scope.stepClick = function(step) {
-        listViewService.getChildrenForStep(step);
-    };
-
-    $scope.getTask = function(branch) {
-        $scope.stepTaskLoading = true;
-        return Task.get({ id: branch.id }).$promise.then(function (data) {
-            if(data.time_started !== null && data.time_done !== null) {
-                var started = moment(data.time_started);
-                var done = moment(data.time_done);
-                data.duration = done.diff(started);
-            } else {
-                data.duration = null;
-            }
-            $scope.currentStepTask = data;
-            $scope.stepTaskLoading = false;
-            return data;
-        });
-    }
-
-    $scope.getStep = function(branch) {
-        $scope.stepTaskLoading = true;
-        return Step.get({ id: branch.id }).$promise.then(function (data) {
-            if(data.time_started !== null && data.time_done !== null) {
-                var started = moment(data.time_started);
-                var done = moment(data.time_done);
-                data.duration = done.diff(started);
-            } else {
-                data.duration = null;
-            }
-            $scope.currentStepTask = data;
-            $scope.stepTaskLoading = false;
-            return data;
-        });
-    }
-    //Click funciton for steps and tasks
-    $scope.stepTaskClick = function (branch) {
-        $scope.stepTaskLoading = true;
-        if (branch.flow_type == "task") {
-            $scope.getTask(branch).then(function(data) {
-                $scope.taskInfoModal();
-            });
-        } else {
-            $scope.getStep(branch).then(function(data) {
-                $scope.stepInfoModal();
-            });
-        }
-    };
-
-    // build comma separated args display string
-    vm.getArgsString = function(args) {
-        return args.map(function(x) {
-            if(x === null) {
-                return 'null';
-            } else {
-                return x;
-            }
-        }).join(', ');
-    }
 
     $scope.checkPermission = function(permissionName) {
         return !angular.isUndefined(PermPermissionStore.getPermissionDefinition(permissionName));
@@ -896,90 +701,6 @@ angular.module('myApp').controller('BaseCtrl',  function(IP, Task, Step, vm, ipS
             return true;
         }
     };
-
-    //Update status view data
-    $scope.statusViewUpdate = function (row) {
-        $scope.statusLoading = true;
-        var expandedNodes = [];
-        if ($scope.tree_data != []) {
-            expandedNodes = checkExpanded($scope.tree_data);
-        }
-        listViewService.getTreeData(row, expandedNodes).then(function (value) {
-            $q.all(value).then(function (values) {
-                if ($scope.tree_data.length) {
-                    $scope.tree_data = updateStepProperties($scope.tree_data, values);
-                } else {
-                    $scope.tree_data = value;
-                }
-            })
-            $scope.statusLoading = false;
-        }, function (response) {
-            if (response.status == 404) {
-                $scope.statusShow = false;
-                $timeout(function () {
-                    $scope.getListViewData();
-                    updateListViewConditional();
-                }, 1000);
-            }
-        });
-    };
-
-    // Calculates difference in two sets of steps and tasks recursively
-    // and updates the old set with the differances.
-    function updateStepProperties(A, B) {
-        if (A.length > B.length) {
-            A.splice(0, B.length);
-        }
-        for (i = 0; i < B.length; i++) {
-            if (A[i]) {
-                for (var prop in B[i]) {
-                    if (B[i].hasOwnProperty(prop) && prop != "children") {
-                        A[i][prop] = compareAndReplace(A[i], B[i], prop);
-                    }
-                }
-                if (B[i].flow_type != "task") {
-                    waitForChildren(A[i], B[i]).then(function (result) {
-                        result.step.children = result.children;
-                    })
-                }
-            } else {
-                A.push(B[i]);
-            }
-        }
-        return A;
-    }
-
-    // Waits for promises in b.children to resolve before returning
-    // the result from updateStepProperties called with children of a and b
-    function waitForChildren(a, b) {
-        return $q.all(b.children).then(function (bchildren) {
-            return { step: a, children: updateStepProperties(a.children, bchildren) };
-        })
-    }
-    // If property in a and b does not have the same value, update a with the value of b
-    function compareAndReplace(a, b, prop) {
-        if (a.hasOwnProperty(prop) && b.hasOwnProperty(prop)) {
-            if (a[prop] !== b[prop]) {
-                a[prop] = b[prop];
-            }
-            return a[prop];
-        } else {
-            return b[prop]
-        }
-    }
-    //checks expanded rows in tree structure
-    function checkExpanded(nodes) {
-        var ret = [];
-        nodes.forEach(function(node) {
-            if(node.expanded == true) {
-                ret.push(node);
-            }
-            if(node.children && node.children.length > 0) {
-                ret = ret.concat(checkExpanded(node.children));
-            }
-        });
-        return ret;
-    }
 
     //Modal functions
     $scope.tracebackModal = function (profiles) {
