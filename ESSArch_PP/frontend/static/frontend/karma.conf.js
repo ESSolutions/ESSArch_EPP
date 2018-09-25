e2a3f1634df89e9b8af8d9fe473ef611c1348c54
petrus <petrus.jonsson@gmail.com>
2018-09-25 15:44:40 +0200

    Remove unneccesary browsers from test browser list

--- ESSArch_PP/frontend/static/frontend/karma.conf.js
+++ ESSArch_PP/frontend/static/frontend/karma.conf.js
@@ -55,7 +55,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome','Chromium', 'Firefox', 'PhantomJS', 'ChromeCanary', 'Safari', 'IE', 'Edge'],
+    browsers: ['Chrome', 'Firefox', 'PhantomJS', 'Safari', 'IE', 'Edge'],
 
     plugins: [
       'karma-chrome-launcher',
