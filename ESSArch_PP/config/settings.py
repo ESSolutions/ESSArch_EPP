5033634305bef433a174fa93a7b6150c8e5d922f
Oskar Persson <oskar.persson@polken.se>
2018-09-26 00:00:36 +0200

    Remove duplicated middleware

--- ESSArch_PP/config/settings.py
+++ ESSArch_PP/config/settings.py
@@ -157,10 +157,9 @@ SITE_ID = 1
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
