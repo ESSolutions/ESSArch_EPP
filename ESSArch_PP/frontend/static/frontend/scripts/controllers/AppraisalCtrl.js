526de81113f585369a3ccdbf7468f3cfe36611fa
petrus <petrus.jonsson@gmail.com>
2018-10-09 09:51:12 +0200

    Remove dead code

--- ESSArch_PP/frontend/static/frontend/scripts/controllers/AppraisalCtrl.js
+++ ESSArch_PP/frontend/static/frontend/scripts/controllers/AppraisalCtrl.js
@@ -149,15 +149,7 @@ angular.module('essarch.controllers').controller('AppraisalCtrl', function(Archi
             });
         }
     }
-    function guid() {
-        function s4() {
-          return Math.floor((1 + Math.random()) * 0x10000)
-            .toString(16)
-            .substring(1);
-        }
-        return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-          s4() + '-' + s4() + s4() + s4();
-      }
+
     /**
      * Run appraisal rule now
      * @param {Object} appraisal
--- ESSArch_PP/frontend/static/frontend/scripts/controllers/ConversionCtrl.js
+++ ESSArch_PP/frontend/static/frontend/scripts/controllers/ConversionCtrl.js
@@ -153,15 +153,7 @@ angular.module('essarch.controllers').controller('ConversionCtrl', function(Arch
             })
         }
     }
-    function guid() {
-        function s4() {
-          return Math.floor((1 + Math.random()) * 0x10000)
-            .toString(16)
-            .substring(1);
-        }
-        return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-          s4() + '-' + s4() + s4() + s4();
-      }
+
     /**
      * Run conversion rule now
      * @param {Object} conversion
