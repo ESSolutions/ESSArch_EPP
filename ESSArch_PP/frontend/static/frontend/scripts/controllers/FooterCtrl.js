f61a05b421abe23ebfa363e6b7a2f07aa6ecfef7
Pettanj <petrus.jonsson@gmail.com>
2018-10-10 16:41:04 +0200

    Remove unused variable

--- ESSArch_PP/frontend/static/frontend/scripts/controllers/FooterCtrl.js
+++ ESSArch_PP/frontend/static/frontend/scripts/controllers/FooterCtrl.js
@@ -1,6 +1,5 @@
 angular.module('essarch.controllers').controller('FooterCtrl', function($scope, $rootScope, $timeout, $translate, $state) {
     var vm = this;
-    var appName = " | ESSArch Preservation Platform";
     vm.pageTitle = "ESSArch Preservation Platform";
     vm.$onInit = function() {
         vm.currentYear = new Date().getFullYear();
