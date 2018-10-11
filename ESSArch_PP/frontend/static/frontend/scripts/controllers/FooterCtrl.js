58e63ec208873d5e2b18b21247686be510e4011b
Pettanj <petrus.jonsson@gmail.com>
2018-10-11 09:57:50 +0200

    Move footer to core

--- ESSArch_PP/frontend/static/frontend/scripts/controllers/FooterCtrl.js
+++
@@ -1,7 +0,0 @@
-angular.module('essarch.controllers').controller('FooterCtrl', function($scope, $rootScope, $timeout, $translate, $state) {
-    var vm = this;
-    vm.pageTitle = "ESSArch Preservation Platform";
-    vm.$onInit = function() {
-        vm.currentYear = new Date().getFullYear();
-    }
-});
