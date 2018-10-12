b742df8fe2038a609a9cf9916b0859fe57fb3658
Pettanj <petrus.jonsson@gmail.com>
2018-10-12 17:04:00 +0200

    Move http config to core

--- ESSArch_PP/frontend/static/frontend/scripts/essarch.module.js
+++ ESSArch_PP/frontend/static/frontend/scripts/essarch.module.js
@@ -680,47 +680,6 @@ angular.module('essarch', [
         }
     });
 }])
-.config(['$httpProvider', '$windowProvider', function($httpProvider, $windowProvider) {
-    var $window = $windowProvider.$get();
-    $httpProvider.defaults.xsrfCookieName = 'csrftoken';
-    $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
-    $httpProvider.interceptors.push(['$q', '$location', '$rootScope', function ($q, $location, $rootScope) {
-        return {
-            'response': function(response) {
-                if($rootScope.disconnected) {
-                    $rootScope.disconnected = false;
-                    $rootScope.$broadcast("reconnected", {detail: "Connection has been restored"});
-                }
-                return response;
-            },
-            'responseError': function(response) {
-                if(response.status == 500) {
-                    var msg = "Internal server error";
-                    if(response.data.detail) {
-                        msg = response.data.detail;
-                    }
-                    $rootScope.$broadcast('add_notification', { message: msg, level: "error", time: null});
-                }
-                if(response.status === 503) {
-                    var msg = "Request failed, try again";
-                    if(response.data.detail) {
-                        msg = response.data.detail;
-                    }
-                    $rootScope.$broadcast('add_notification', { message: msg, level: "error", time: null});
-                }
-                if((response.status === 401 || response.status === 403) && !response.config.noAuth) {
-                    if ($location.path() != '/login' && $location.path() != ''){
-                        $window.location.assign('/');
-                    }
-                }
-                if(response.status <= 0) {
-                    $rootScope.$broadcast("disconnected", {detail: "Lost connection to server"});
-                }
-                return $q.reject(response);
-            }
-        };
-    }]);
-}])
 .config(['$uibTooltipProvider', function($uibTooltipProvider) {
     var parser = new UAParser();
     var result = parser.getResult();
