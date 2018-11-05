953dcacd4ec3657f994bef313e3f7d1737cc0049
Petrus Jonsson <petrus@essolutions.se>
2018-11-05 21:43:33 +0100

    Move Import component to core (#40)

--- ESSArch_PP/frontend/static/frontend/scripts/components/ImportComponent.js
+++
@@ -1,7 +0,0 @@
-angular.module('essarch.components').component('import', {
-  templateUrl: 'static/frontend/views/import.html',
-  controller: 'ImportCtrl',
-  controllerAs: 'vm',
-  bindings: {
-  }
-});
