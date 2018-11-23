80bea98e10f249960abeef2f7551c293275a10e7
Pettanj <petrus.jonsson@gmail.com>
2018-11-23 11:58:36 +0100

    Move event table to core

--- ESSArch_PP/frontend/static/frontend/scripts/components/EventTableComponent.js
+++
@@ -1,8 +0,0 @@
-angular.module('essarch.components').component('eventTable', {
-    templateUrl: 'static/frontend/views/event_table.html',
-    controller: 'EventCtrl',
-    controllerAs: 'vm',
-    bindings: {
-        ip: "<"
-    }
-  });
