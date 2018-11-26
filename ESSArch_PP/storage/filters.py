9deed4d6fc3a7ffa94a27ee72ab6a26c7971032c
Oskar Persson <oskar.persson@polken.se>
2018-11-26 15:35:29 +0100

    Add storage_type filter to storage mediums API (#68)

--- ESSArch_PP/storage/filters.py
+++ ESSArch_PP/storage/filters.py
@@ -25,12 +25,14 @@
 from django_filters import rest_framework as filters
 
 from ESSArch_Core.filters import ListFilter
-from ESSArch_Core.storage.models import StorageMedium
+from ESSArch_Core.storage.models import StorageMedium, medium_type_CHOICES, storage_type_CHOICES
 
 
 class StorageMediumFilter(filters.FilterSet):
     status = ListFilter(field_name='status', distinct='true')
+    medium_type = filters.ChoiceFilter(field_name='storage_target__type', choices=medium_type_CHOICES)
+    storage_type = filters.ChoiceFilter(field_name='storage_target__storage_method_target_relations__storage_method__type', choices=storage_type_CHOICES)
 
     class Meta:
         model = StorageMedium
-        fields = ('status',)
+        fields = ('status', 'medium_type', 'storage_type')
