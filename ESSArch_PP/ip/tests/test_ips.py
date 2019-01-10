f3a684c85fc25a8ffdca2197c9159a0df7896de4
Oskar Persson <oskar.persson@polken.se>
2019-01-10 16:13:30 +0100

    Use built-in mock (#104)

--- ESSArch_PP/ip/tests/test_ips.py
+++ ESSArch_PP/ip/tests/test_ips.py
@@ -32,7 +32,7 @@ from django.urls import reverse
 
 from groups_manager.models import GroupType
 
-import mock
+from unittest import mock
 
 from rest_framework import status
 from rest_framework.response import Response
