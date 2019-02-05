58e21826170fb615292329a60338d1ea03da0eec
Petrus Jonsson <petrus@essolutions.se>
2019-02-05 10:32:36 +0100

    Fix translation issue when selecting tags
    Remove old function for building translation strings

--- ESSArch_PP/frontend/static/frontend/scripts/controllers/TagsCtrl.js
+++ ESSArch_PP/frontend/static/frontend/scripts/controllers/TagsCtrl.js
@@ -38,30 +38,6 @@ angular
       };
     };
 
-    $scope.tagsPlaceholder = function(type) {
-      if (
-        (!$scope.tagsLoading && type.toUpperCase() == 'ARCHIVE' && vm.tags.archive.options.length == 0) ||
-        (!$scope.structuresLoading &&
-          type.toUpperCase() == 'CLASSIFICATION_STRUCTURE' &&
-          vm.tags.structure.options.length == 0) ||
-        (!$scope.descendantsLoading &&
-          type.toUpperCase() == 'STRUCTURE_UNIT' &&
-          vm.tags.descendants.options.length == 0)
-      ) {
-        if (type) {
-          return 'NO_' + type.toUpperCase() + 'S';
-        } else {
-          return 'NO_TAGS';
-        }
-      } else {
-        if (type) {
-          return 'SELECT_' + type.toUpperCase() + '_TAG';
-        } else {
-          return 'SELECT_TAGS';
-        }
-      }
-    };
-
     $scope.getArchives = function(search) {
       return $http({
         method: 'GET',
