20911e625a4b99bc6715a1da554b29a47001e54a
Petrus Jonsson <petrus@essolutions.se>
2019-04-15 16:18:36 +0200

    Fix spinner when pubishing structure

--- ESSArch_PP/frontend/static/frontend/scripts/controllers/PublishClassificationStructureCtrl.js
+++ ESSArch_PP/frontend/static/frontend/scripts/controllers/PublishClassificationStructureCtrl.js
@@ -6,12 +6,18 @@ angular
       $ctrl.data = data;
     };
     $ctrl.publish = function() {
+      $ctrl.publishing = true;
       $http({
         url: appConfig.djangoUrl + 'structures/' + data.structure.id + '/publish/',
         method: 'POST',
-      }).then(function(response) {
-        $uibModalInstance.close(response);
-      });
+      })
+        .then(function(response) {
+          $ctrl.publishing = false;
+          $uibModalInstance.close(response);
+        })
+        .catch(function() {
+          $ctrl.publishing = false;
+        });
     };
     $ctrl.cancel = function() {
       $uibModalInstance.dismiss('cancel');
